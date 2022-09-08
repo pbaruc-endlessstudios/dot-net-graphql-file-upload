c198f35d12309438291d9369e465fafdbafefa89
pbaruc-endlessstudios <pbaruc@endlessstudios.com>
2022-09-08 16:05:35 -0700

    Adding comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,49 +3,13 @@ using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
 using System.Security.Cryptography.X509Certificates;
 
+
+// GraphQL Client to the development environment
 var graphQLClient = new GraphQLHttpClient("https://dev.graphql-api.endlessstudios.com/graphql", new NewtonsoftJsonSerializer());
-/*graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer")
-*/
-/*var loginWithUsernameOrEmail = new GraphQLRequest
-{
-Query = @"query Check {
-        check {
-            systemHealthy
-        }
-    }",
-OperationName = "Check"
-};
 
 
-
-
-var graphQLResponse = await graphQLClient.SendQueryAsync<ResponseType>(loginWithUsernameOrEmail);
-
-var token = graphQLResponse.Data;
-
-Console.WriteLine("Token: " + token);
-
-public class CheckType
-{
-    public bool systemHealthy { get; set; }
-}
-public class ResponseType
-{
-    public CheckType check { get; set; }
-}
-*/
-
-
-/*public class CheckType
-{
-    public bool systemHealthy { get; set; }
-}
-public class ResponseType
-{
-    public CheckType check { get; set; }
-}
-*/
-
+// Create a GraphQL Request that invokes the loginWithUsernameOrEmail graphql mutation
+// pass in a username or email and a password and recieve back and auth token.
 var loginWithUsernameOrEmail = new GraphQLRequest
 {
 Query = @"mutation loginWithUsernameOrEmail($username_or_email: String!, $password: String!) {
@@ -57,21 +21,52 @@ Query = @"mutation loginWithUsernameOrEmail($username_or_email: String!, $passwo
     OperationName = "loginWithUsernameOrEmail",
     Variables = new
     {
-        username_or_email = "user0",
+        username_or_email = "user0",    // This is an existing test account we have establised in dev environment
         password = "12345678"
     }
 };
 
 
+// Invoke the query against service, retrieve response
+var graphQLResponse = await graphQLClient.SendQueryAsync<LoginWithUsernameOrEmailResponseType>(loginWithUsernameOrEmail);
+
+// Extract the authToken from the the response like this... 
+var authToken = graphQLResponse.Data.loginWithUsernameOrEmail;
+
+// Now store the authToken into an 'Authorization' request header in graphQLClient...
+graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + authToken);
+
+// ====================================================================
+// Now all subsequent queries will execute queries using the auth token
+// ====================================================================
+
+// Invoke 'me' graphql query to return details for the authorized user.
+var me = new GraphQLRequest
+{
+    Query = @"query me {
+        me {
+            id
+            username
+            email
+        }
+    }",
+    OperationName = "me"
+};
 
 
-var graphQLResponse = await graphQLClient.SendQueryAsync<ResponseType>(loginWithUsernameOrEmail);
+// ================================================================================================================
 
-var token = graphQLResponse.Data.loginWithUsernameOrEmail;
-
-Console.WriteLine("Token: " + token);
-
-public class ResponseType
+public class LoginWithUsernameOrEmailResponseType
 {
     public string loginWithUsernameOrEmail { get; set; }
 }
+
+public class MeResponseType
+{
+    public int id { get; set; }
+
+    public string username { get; set; }
+
+    public string email { get; set; }
+}
+
